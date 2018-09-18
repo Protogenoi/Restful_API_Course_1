a34a614e59dee5b40c8a249076c4a28acb69df48
Michael <michael@adl-crm.uk>
2018-09-18 14:47:29 +0100

    Completed Chapter 8: Updating the Location Header

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -16,7 +16,8 @@ class ProgrammerController extends BaseController
     {
         $controllers->post('/api/programmers', array($this, 'newAction'));
         $controllers->get('/api/programmers/{nickname}',
-            array($this, 'showAction'));
+            array($this, 'showAction'))
+            ->bind('api_programmers_show');
     }
 
     public function newAction(Request $request)
@@ -32,8 +33,12 @@ class ProgrammerController extends BaseController
 
         $this->save($programmer);
 
+        $url = $this->generateUrl('api_programmers_show', array(
+            'nickname' => $programmer->nickname,
+        ));
+
         $response = new Response('It worked! Trust me, I\'m an API', 201);
-        $response->headers->set('Location', '/programmers/not/fake...');
+        $response->headers->set('Location', $url);
         return $response;
     }
 
